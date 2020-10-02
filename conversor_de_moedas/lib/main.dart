caedd872101011fd9ccacce95cad36deccc6f52d
Matheus Xavier <mattheus.x.a@gmail.com>
2020-10-02 00:18:50 -0300

    First Commit

---
+++ conversor_de_moedas/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'dart:html';
+import 'dart:async';
+
+const request =
+    "https://api.hgbrasil.com/finance?format=json-cors&key=0e5189b6";
+
+void main() {
+  runApp(MaterialApp(home: Container()));
+}
