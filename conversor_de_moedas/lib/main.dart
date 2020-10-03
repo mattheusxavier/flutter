bb8d763bbb2046209b11e54e3e3304024c0b867e
Matheus Xavier <mattheus.x.a@gmail.com>
2020-10-03 20:43:34 -0300

    Future

--- conversor_de_moedas/lib/main.dart
+++ conversor_de_moedas/lib/main.dart
@@ -1,13 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'dart:async';
 
 const request =
     "https://api.hgbrasil.com/finance?format=json-cors&key=0e5189b6";
 
 void main() async {
-  http.Response response = await http.get(request);
-  print(json.decode(response.body)["results"]["currencies"]["USD"]);
+  runApp(MaterialApp(home: Home()));
+}
 
-  runApp(MaterialApp(home: Container()));
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+Future<Map> getData() async {
+  http.Response response = await http.get(request);
+  return json.decode(response.body);
 }
