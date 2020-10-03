9eea15ce1d4dfbd3dd34ecae5d32b29bbe733be4
Matheus Xavier <mattheus.x.a@gmail.com>
2020-10-03 20:35:08 -0300

    Requisições API HG

--- conversor_de_moedas/lib/main.dart
+++ conversor_de_moedas/lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
-import 'dart:html';
-import 'dart:async';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 const request =
     "https://api.hgbrasil.com/finance?format=json-cors&key=0e5189b6";
 
-void main() {
+void main() async {
+  http.Response response = await http.get(request);
+  print(json.decode(response.body)["results"]["currencies"]["USD"]);
+
   runApp(MaterialApp(home: Container()));
 }
